03197551bf008da759b48a3216274aa4f8b3ba75
Victor Twu <victor607@gmail.com>
2021-03-20 14:18:41 -0700

    connected add to cart button and got it to create one cart item, got to figure out how to make into array and render on cart page

--- controllers/cart_controller.js
+++ controllers/cart_controller.js
@@ -9,15 +9,47 @@ const Cart = require('../models/cart')
 // routes
 
 // get cart page
-router.get('/', (req, res) => {
-  res.render('cart.ejs')
+router.get('/:id', (req, res) => {
+  Cart.findById(req.params.id, (err, foundItem) => {
+    res.render('cart.ejs', {
+      cartItem: foundItem,
+      currentUser: req.session.currentUser
+    })
+  })
 })
 // edit route - PUT
 
 // delete route - DELETE
 
-// Add to cart route - POST
+// Add to cart route - PUT
 // this route will push item to cart and redirect to show page (bc that's where the user is)
+router.post('/:id', (req, res) => {
+  console.log(req.body)
+  // const { productId, name, description, img, price, availability } = req.body
+  // console.log(req.body)
+  const newCartItem = Cart.create(req.body
+    // cartItems: [
+      // productId: productId,
+      // name: name,
+      // description: description,
+      // img: img,
+      // price: price,
+      // availability: availability
+      // }]
+  , (err, result)=> {
+    console.log(result)
+    // Cart.cartItems.push(result)
+    res.redirect('/cart/' + req.params.id)
+  })
+
+  // newCartItem.save()
+  //for item document use model.create()??
+
+})
+
+
+
+
 
 // buy route - PUT
 // this route will redirect to 'reciept' page
--- models/cart.js
+++ models/cart.js
@@ -2,10 +2,45 @@ const mongoose = require('mongoose')
 const { Schema, model } = mongoose
 
 
+// let cartItemSchema = new Schema ({
+//   productId: {
+//     type: mongoose.Schema.Types.ObjectId,
+//
+//   }
+// })
+
+
+
+//
+// const cartSchema = new Schema({
+//   userId: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'User'
+//   },
+//   cartItems: [
+//     {
+//       productId: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Item'
+//       },
+//       name: String,
+//       description: String,
+//       img: String,
+//       price: Number,
+//       availability: Boolean
+//     }
+//   ]
+// })
+
 const cartSchema = new Schema({
-  name: String,
-  price: Number
+  name: { type: String, required: true },
+  description: String,
+  img: String,
+  price: { type: Number, required: true },
+  availability: { type: Boolean, default: true }
 })
+
+
 
 const Cart = model('Cart', cartSchema)
 
